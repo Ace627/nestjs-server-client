eafa4059b31a2bfd9febb25987dd0d3b8776a29d
当时只道是寻常 <1207588603@qq.com>
2024-11-14 13:51:20 +0800

    feat: 旋转太极图示例

---
+++ src/views/GlobalCom/TaiChi/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app-content flex flex-col items-center justify-center h-full overflow-hidden">
+    <div class="flex mb-32px">
+      <div class="flex-center">
+        <span class="mr-4px">浅色</span>
+        <el-color-picker v-model="lightColor" />
+      </div>
+      <div class="flex-center ml-16px">
+        <span class="mr-4px">深色</span>
+        <el-color-picker v-model="darkColor" />
+      </div>
+      <div class="flex-center ml-16px w-220px">
+        <span class="flex-none mr-20px">速度</span>
+        <el-slider v-model="speed" :max="10" :min="0.1" />
+      </div>
+    </div>
+
+    <ApTaiChi :lightColor :darkColor :speed />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({ name: 'TaiChi' })
+
+const lightColor = ref('#fff')
+const darkColor = ref('#000')
+const speed = ref(1)
+</script>
+
+<style lang="scss" scoped></style>
